c094f612058334a4baf8911c1cd6fbc3c900f6f5
Ehud <ehud@playedonline.com>
2011-11-10 16:10:59 +0200

    small fix

--- lib/sorcery/model/submodules/external.rb
+++ lib/sorcery/model/submodules/external.rb
@@ -40,9 +40,9 @@ module Sorcery
         module ClassMethods
           # takes a provider and uid and finds a user by them.
           def load_from_provider(provider,uid)
-            config = sorcery_config
+             config = sorcery_config
             authentication = config.authentications_class.find_by_provider_and_uid(provider, uid)
-            user = find(authentication.send(config.authentications_user_id_attribute_name)) if authentication
+            user = from_cache(:id => authentication.send(config.authentications_user_id_attribute_name)) if authentication
           end
         end
         
